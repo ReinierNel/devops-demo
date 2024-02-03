bc92ac515603675de3c851a81669c61219d52075
Reinier Nel <reinier.nel@bma.network>
2024-02-03 14:24:32 +0200

    updated game

--- src/game/index.service.worker.js
+++ src/game/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1706452924|83287488456";
+const CACHE_VERSION = "1706963036|383654994";
 const CACHE_PREFIX = "Tripple-t-x 3-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
